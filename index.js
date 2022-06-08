498e973c6e63060d039de7395acfd0d8d538ba1d
mahmoud <mahmoud.mohamed30120@gmail.com>
2022-06-08 20:12:13 +0200

    feat: save function

--- index.js
+++ index.js
@@ -2,15 +2,30 @@ let counter = 0;
 
 function increment() {
   counter++;
-  document.getElementById("counter").innerHTML = counter;
+  document.getElementById("counter").innerText = counter;
 }
 
 function decrement() {
   counter--;
-  document.getElementById("counter").innerHTML = counter;
+  document.getElementById("counter").innerText = counter;
 }
 
 function reset() {
     counter = 0;
-    document.getElementById("counter").innerHTML = counter;
+    document.getElementById("save").innerText = '';
+    document.getElementById("counter").innerText = counter;
+  }
+
+  function save() {
+    if (document.getElementById("save").innerText == '') {
+      let counterStr =  counter.toString();
+      document.getElementById("save").innerText += counterStr;
+    }else
+    {   
+      let counterStr =  '-'+ counter
+      document.getElementById("save").innerText += counterStr;
+
+ 
+    }
+
   }
